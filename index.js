460ea156556b49d2a61ab5f94e42da94f5c4d6f8
{B-Lubanga} <{Lubangabenjamin@gmail.com}>
2025-03-21 03:12:27 +0300

    eventlistener added on js file

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+  const button = document.getElementById("button"); // This is selecting the element "button"
+  button.addEventListener("click", function () {
+    alert("Button clicked!");
+  });
 }
